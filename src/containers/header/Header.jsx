cf0dd562beaf14727bb1371a6e4451bc5227df0e
ega-erinovian <erinovian69@gmail.com>
2022-09-20 20:42:28 +0700

    initial commit + header section

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <section className="header">
+        <div className="container_padding">
+            <h1>I'am Ega Erinovian</h1>
+            <p>Full-Stack Web Developer</p>
+        </div>
+    </section>
+  )
+}
+
+export default Header
