10ed98a6bc625226647b8f6c1b3bcae3e1d1bd88
ega-erinovian <erinovian69@gmail.com>
2022-10-18 13:07:18 +0700

    created about component for reuseable component

---
+++ src/components/about/AboutComp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './about.css';
+
+const About = ({title, content1, content2=""}) => {
+  return (
+    <section className="about" id="about">
+        <div className="container_padding">
+          <div className="about_content">
+            <h1>{title}</h1>
+            <p>{content1} <br/><br/> {content2}</p>
+          </div>
+        </div>
+    </section>
+  )
+}
+
+export default About
--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -1,21 +1,9 @@
-
 import React from 'react'
-import './about.css';
+import AboutComp from '../../components/about/AboutComp'
 
 const About = () => {
   return (
-    <section className="about" id="about">
-        <div className="container_padding">
-          <div className="about_content">
-            <h1>About</h1>
-            <p>
-              I am Ega Erinovian Megananda Putra, 6th semester student majoring in Informatics Engineering at Universitas Pembangunan Nasional "Veteran" Yogyakarta. <br/> <br/>
-
-              I have experience in web development for 2 years. I also have the ability to design user interfaces for web applications and mobile applications. I am able to work well individually and in a team, as well as diligent and honest in doing work and assignments.
-            </p>
-          </div>
-        </div>
-    </section>
+    <AboutComp title="About" content1={'I am Ega Erinovian Megananda Putra, 6th semester student majoring in Informatics Engineering at Universitas Pembangunan Nasional "Veteran" Yogyakarta.'} content2={'I have experience in web development for 2 years. I also have the ability to design user interfaces for web applications and mobile applications. I am able to work well individually and in a team, as well as diligent and honest in doing work and assignments.'}/>
   )
 }
 
